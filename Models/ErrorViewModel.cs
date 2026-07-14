1676e7b84c767d49525690ed33314e2a6526fd02
ayusha-ui <ayushakarki004@gmail.com>
2026-07-14 09:14:42 +0545

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Blood_Donation_Management.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
